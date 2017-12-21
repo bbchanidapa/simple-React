8a0bf6ff9980df440dc87159d6a01e64c8321081
bbchanidapa <chanidapa.v@scale360solutions.com>
2017-12-21 16:51:46 +0700

    edit sytle code

--- src/Card/CardContent/CardContentContainer.js
+++ src/Card/CardContent/CardContentContainer.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 
-class CardContentContainer extends Component {
-  render() {
-    return (
-      <div>
-        <p style={{color: 'pink', fontSize: 10}}>
-          {this.props.description}
-        </p>
-      </div>
-    )
-  }
+const CardContentContainer = (props) => {
+  return (
+    <div>
+      <p style={{color: 'pink', fontSize: 10}}>
+        {props.description}
+      </p>
+    </div>
+  )
 }
 
 export default CardContentContainer
