29a06a589676cb95915b28515584f614c196afc3
bbchanidapa <chanidapa.v@scale360solutions.com>
2017-12-21 16:47:04 +0700

    style code

--- src/Card/Images/ImagesContainer.js
+++ src/Card/Images/ImagesContainer.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
-class ImagesContainer extends Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} style={{width: "50px", height: "50px"}}/>
-      </div>
-    )
-  }
+const ImagesContainer = (props) => {
+  return (
+    <div>
+      <img src={props.image} style={{width: "50px", height: "50px"}}/>
+    </div>
+  )
 }
 
 export default ImagesContainer
