6018b474a527d642a34cb0505c90fc91ec2eb1cf
Borbee <chanidapa.v@scale360solutions.com>
2018-01-19 18:29:10 +0700

    line noti

--- src/getApi.js
+++ src/getApi.js
@@ -1,9 +1,30 @@
 var axios = require('axios')
 const url = 'https://bx.in.th/api/'
-
-setInterval(function(){
-    axios.get(url).then((res = {})=> {
-        if(res.data['25'].last_price >= 75) console.log(res.data['25'].last_price) 
-        else if(res.data['25'].last_price <= 67) console.log(res.data['25'].last_price) 
-    })
-},2000)
+const token = '8v0iqsNBiu9ZLHHfWp8ZicmEgLbUlNoCxjru2RnnzMg'
+// setInterval(function(){
+//     axios.get(url).then((res = {})=> {
+//         if(res.data['25'].last_price >= 75) console.log('XRP >>>',res.data['25'].last_price) 
+//         else if(res.data['25'].last_price <= 56) console.log('XRP <<<', res.data['25'].last_price)
+//         // EVX
+//         else if(res.data['28'].last_price <= 157) console.log('EVX BUY NOW....', res.data['28'].last_price)
+//         else if(res.data['28'].last_price >= 165) console.log('>>>>>EVX SELL<<<<<', res.data['28'].last_price)    
+//         console.log('EVX', res.data['28'].last_price)
+//     })
+   
+// },2000)
+// axios.post({url:'https://notify-api.line.me/api/notify'}).then((res = {})=> {
+    
+// })
+axios({
+    method: 'POST',
+    uri: 'https://notify-api.line.me/api/notify',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+        'bearer': token
+    },
+    form: {
+      message: 'message'
+    }
+})
